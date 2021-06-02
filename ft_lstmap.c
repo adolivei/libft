f15fe69b03afc797dbdfa0ffdf3deabf8155a97e
Adolfo Oliveira <adolivei@c1r13s1.42lisboa.com>
2021-06-02 16:42:02 +0100

    💽

---
+++ ft_lstmap.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adolivei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/02 15:05:52 by adolivei          #+#    #+#             */
+/*   Updated: 2021/03/02 15:25:11 by adolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list *new_node;
+	t_list *new_list;
+
+	if (!f || !lst)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		if (!(new_node = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
---
+++ ft_substr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adolivei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/02 18:09:49 by adolivei          #+#    #+#             */
+/*   Updated: 2021/03/02 18:16:59 by adolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*str;
+	unsigned int	i;
+	unsigned int	j;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		str = malloc(sizeof(char));
+		*str = '\0';
+		return (str);
+	}
+	if (len > ft_strlen(s))
+		return (ft_strdup(s + start));
+	if (!(str = (char*)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	i = start;
+	j = 0;
+	while (i < ft_strlen(s) && j < len)
+		str[j++] = s[i++];
+	str[j] = '\0';
+	return (str);
+}
